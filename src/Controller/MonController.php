022741467a2dc06fbdd3909aae762c33c9ca1210
Falk Rund <Un1matr1x@users.noreply.github.com>
2021-03-06 07:46:12 +0100

    [main] add basic MonController

---
+++ src/Controller/MonController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MonController extends AbstractController
+{
+    #[Route('/mon', name: 'mon')]
+    public function index(): Response
+    {
+        return $this->render('mon/index.html.twig', [
+            'controller_name' => 'MonController',
+        ]);
+    }
+}
